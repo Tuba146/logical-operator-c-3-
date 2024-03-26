a505456d2798be114d3ac1e561c6f88a5fc02ba3
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-03-26 15:08:52 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Logical operators
+var isBookAvalaibel = true;
+var userBorrowedBook = 2;
+var maxAllowedBooks = 2;
+if (isBookAvalaibel && userBorrowedBook < maxAllowedBooks) {
+    console.log("ap book borrow kr skty hain");
+}
+else {
+    console.log("ap book borrow nhi kr skty");
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Logical operators
+
+let isBookAvalaibel: boolean = true
+let userBorrowedBook: number = 2
+const maxAllowedBooks: number = 2
+
+if(isBookAvalaibel && userBorrowedBook < maxAllowedBooks) {
+    console.log("ap book borrow kr skty hain")
+} else {
+    console.log("ap book borrow nhi kr skty")
+}
+// ap book borrow nhi krskte
